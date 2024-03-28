845e9166361030f9f21300b15d21aa789ff684de
csamga <gabin.mascioli@gmail.com>
2024-03-28 23:12:38 +0100

    ajout module dédié fonctions terminal

---
+++ src/terminal.c
@@ -0,0 +1,48 @@
+#include "terminal.h"
+
+#include <stdio.h>
+
+#define CSI "\x1b["
+
+void clear_screen(void) {
+    fputs(CSI "2J", stdout);
+}
+
+void use_screen_buffer(enum screen_buffer sb) {
+    switch (sb) {
+    case screen_buffer_alternative:
+        fputs(CSI "?1049h", stdout);
+        break;
+    case screen_buffer_default:
+        fputs(CSI "?1049l", stdout);
+        break;
+    }
+}
+
+void set_cursor_pos(short x, short y) {
+    printf(CSI "%hd;%hdH", ++y, ++x);
+}
+
+void move_cursor_to_col(short x) {
+    printf(CSI "%hdG", x);
+}
+
+void set_cursor_home(void) {
+    fputs(CSI "H", stdout);
+}
+
+void save_cursor_pos(void) {
+    fputs(CSI "s", stdout);
+}
+
+void restore_cursor_pos(void) {
+    fputs(CSI "u", stdout);
+}
+
+void show_cursor(bool show) {
+    if (show) {
+        fputs(CSI "?25h", stdout);
+    } else {
+        fputs(CSI "?25l", stdout);
+    }
+}
---
+++ src/terminal.h
@@ -0,0 +1,20 @@
+#ifndef TERMINAL_H_INCLUDED
+#define TERMINAL_H_INCLUDED
+
+#include <stdbool.h>
+
+enum screen_buffer {
+    screen_buffer_default,
+    screen_buffer_alternative
+};
+
+void clear_screen(void);
+void use_screen_buffer(enum screen_buffer sb);
+void set_cursor_pos(short x, short y);
+void move_cursor_to_col(short x);
+void set_cursor_home(void);
+void save_cursor_pos(void);
+void restore_cursor_pos(void);
+void show_cursor(bool show);
+
+#endif
