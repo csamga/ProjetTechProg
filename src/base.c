955b15c50f2e99bcf8d7b09402b54b85c2cbfe29
csamga <gabin.mascioli@gmail.com>
2024-03-28 23:11:34 +0100

    ajout fonctions haut niveau pour actions de base

---
+++ src/base.c
@@ -0,0 +1,45 @@
+#include "base.h"
+
+#include "client.h"
+#include "fournisseur.h"
+#include "produit.h"
+
+void base_register(enum modes mode) {
+    switch (mode) {
+    case MODE_CLIENT:
+        client_register();
+        break;
+    case MODE_SUPPLIER:
+        fournisseur_register();
+        break;
+    case MODE_PRODUCT:
+        product_register();
+        break;
+    case MODE_SALE:
+        puts("implémenter enregistrer transac");
+        getchar();
+        break;
+    case MODE_QUIT:
+        break;
+    }
+}
+
+void base_inspect(enum modes mode) {
+    switch (mode) {
+    case MODE_CLIENT:
+        client_inspect();
+        break;
+    case MODE_SUPPLIER:
+        fournisseur_inspect();
+        break;
+    case MODE_PRODUCT:
+        product_inspect();
+        break;
+    case MODE_SALE:
+        puts("implémenter consulter histo ventes");
+        getchar();
+        break;
+    case MODE_QUIT:
+        break;
+    }
+}
---
+++ src/base.h
@@ -0,0 +1,15 @@
+#ifndef BASE_H_INCLUDED
+#define BASE_H_INCLUDED
+
+enum modes {
+    MODE_CLIENT,
+    MODE_SUPPLIER,
+    MODE_PRODUCT,
+    MODE_SALE,
+    MODE_QUIT
+};
+
+void base_register(enum modes mode);
+void base_inspect(enum modes mode);
+
+#endif
